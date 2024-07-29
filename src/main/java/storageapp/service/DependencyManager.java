1a4b756ffb87568225be55118f6e767948c0d010
Aya Talbi El Alami <talbielalamiaya@gmail.com>
2024-07-29 11:34:26 +0100

    New jar

--- src/main/java/storageapp/service/DependencyManager.java
+++ src/main/java/storageapp/service/DependencyManager.java
@@ -1,20 +1,13 @@
 package storageapp.service;
 
-import javafx.stage.FileChooser;
 import storageapp.StorageApp;
 import storageapp.model.*;
 import storageapp.repository.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.URI;
+
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
 import java.sql.*;
 
-import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 public class DependencyManager {
     private final FactureRepository factureRepository;
@@ -39,24 +32,13 @@ public class DependencyManager {
 
         /* Créer la base de données */
         //String jdbcUrl = "jdbc:sqlite:/C:\\Users\\NEWBOSS\\OneDrive\\Documents\\Logiciel de stockage\\sqlite-tools-win-x64-3460000\\storage.db";
-        //URL bd = StorageApp.class.getResource("storage.db");
+        System.out.println("Hello");
+        String bd = String.valueOf(StorageApp.class.getResource("storage.db"));
+        String modified_bd = bd.substring(4);
+        System.out.println(modified_bd);
 
-        String cleanPath = null;
-        try {
-            URL bd = StorageApp.class.getResource("storage.db");
-            if (bd != null) {
-                URI uri = bd.toURI();
-                cleanPath = Paths.get(uri).toString();
-            } else {
-                System.out.println("Resource not found.");
-            }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        //System.out.println(bd);
-        //String modified_bd = bd.replaceFirst("^.{4}", "");
-        //System.out.println(modified_bd);
-        connection = DriverManager.getConnection("jdbc:sqlite:/" + cleanPath);
+
+        connection = DriverManager.getConnection("jdbc:sqlite" + modified_bd);
         connection.setAutoCommit(false);
 
         factureRepository = new InMemoryFactureRepo(connection);
